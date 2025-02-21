74ede9ef699832c7dea11f7f18896c7950f59393
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-02-21 04:07:15 +0300

    Create sketch_feb21.ino

---
+++ AUaRP/sketch_feb21.ino
@@ -0,0 +1,22 @@
+int val ; // to store incoming serial data - to store 'R'
+int ledpin = 13 ; //define the digital interface 13 - assign digital pin 13 to 'ledpin'
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (9600) ;
+  pinMode (ledpin, OUTPUT) ;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = Serial.read () ;
+  if (val == 'R') 
+  {
+    digitalWrite (ledpin, HIGH) ;
+    delay (500) ;
+    digitalWrite (ledpin, LOW) ;
+    delay (500) ;
+    Serial.println ("Hello W!") ;
+  }
+}
