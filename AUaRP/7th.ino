16d385488a4e7ad3edfb918d1d2e6d4e84965db1
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-03-06 22:10:54 +0300

    loops added

--- AUaRP/7th.ino
+++ AUaRP/7th.ino
@@ -7,5 +7,22 @@ void setup ()
 
 void loop () 
 {
-  
+  unsigned char i, j ;
+  while (1) 
+    {
+      for (i = 0; i <80; i++)
+        {
+          digitalWrite (buzzer, HIGH) ;
+          delay (1) ;
+          digitalWrite (buzzer, LOW) ;
+          delay (1);
+        }
+      for (i = 0; i <100; i++) 
+      {
+        digitalWrite (buzzer, HIGH) ;
+        delay (2) ;
+        digitalWrite (buzzer, LOW) ;
+        delay (2) ;
+      }
+    }
 }
