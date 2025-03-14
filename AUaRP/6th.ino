a8071db481dce796033533e97176fc65c86bb6fe
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-03-14 20:44:25 +0300

    Update 6th.ino

--- AUaRP/6th.ino
+++ AUaRP/6th.ino
@@ -19,5 +19,8 @@ void loop ()
   digitalWrite (greenLed, HIGH) ;
   delay (1000) ;
   digitalWrite (greenLed, LOW) ;
+    digitalWrite (greenLed, HIGH) ;
+  delay (200) ;
+  digitalWrite (greenLed, LOW) ;
 }
 
