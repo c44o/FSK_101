5c8f10d790a755b717a402391a0cd6189c4a316c
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-05-01 21:53:25 +0300

    loop added

--- AUaRP/16_voice_recognizer.ino
+++ AUaRP/16_voice_recognizer.ino
@@ -6,3 +6,12 @@ void setup () {
   pinMode (ledPin, OUTPUT);
   Serial.begin (9600);
 }
+
+void loop () {
+  sensorValue = analogRead (sensorPin);
+  digitalWrite (ledPin, HIGH);
+  delay (sensorValue);
+  digitalWrite (ledPin, LOW);
+  delay (sensorValue);
+  Serial.println(sensorValue, DEC);
+}
