83e8b356f736ca758a550b4747a415e706b4c185
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-05-05 17:17:27 +0300

    Create 18_indicator.ino

---
+++ AUaRP/18_indicator.ino
@@ -0,0 +1,21 @@
+int a = 4;
+int b = 5;
+int c = 6; 
+int d = 7;
+int e = 8;
+int f = 9;
+int g = 10;
+int dp = 11;
+
+void digital_1 (void) 
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  digitalWrite(dp, LOW);
+}
+  
