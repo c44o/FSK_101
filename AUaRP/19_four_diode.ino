02e2b220089dcaf3b884fba304509d8909b73188
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-05-19 22:57:37 +0300

    Create 19_four_diode.ino

---
+++ AUaRP/19_four_diode.ino
@@ -0,0 +1,21 @@
+int a = 8 ;
+int b = 7 ;
+int c = 6 ;
+int d = 5 ;
+int e = 4 ;
+int f = 3 ;
+int g = 2 ;
+int p = 1 ;
+
+int d4 = 9 ;
+int d3 = 10 ;
+int d2 = 11 ;
+int d1 = 12 ;
+
+long n = 0 ; 
+int x = 100 ;
+int del = 55 ;
+
+void setup() 
+{
+}
