4e94aff5676bee62bdfe67a9818ad931b04cc176
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-09-08 23:34:33 +0300

    Create 20_diode_matrix.ino

---
+++ AUaRP/20_diode_matrix.ino
@@ -0,0 +1,23 @@
+const int row1 = 2;
+const int row2 = 3;
+const int row3 = 4;
+const int row4 = 5;
+const int row5 = 17;
+const int row6 = 16;
+const int row7 = 15;
+const int row8 = 14;
+
+const int col1 = 6;
+const int col2 = 7;
+const int col3 = 8;
+const int col4 = 9;
+const int col5 = 10;
+const int col6 = 11;
+const int col7 = 12;
+const int col8 = 13;
+
+void setup () {
+int i = 0;
+  for (i = 2;  i<18; i++) {
+  }
+}
