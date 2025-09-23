75b5b8d387b7ca6547af21d2c11b8c7efa030648
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-09-23 22:41:51 +0300

    Create 21_color_diode.ino

---
+++ AUaRP/21_color_diode.ino
@@ -0,0 +1,35 @@
+int ledPin = 13;
+int redPin = 11;
+int greenPin = 9;
+int bluePin = 10;
+
+void setup () 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop () {
+  color (255, 0, 0);
+  delay(1000);
+  color (0, 255, 0);
+  delay(1000);
+  color (0, 0, 255);
+  delay(1000);
+  color (255, 255, 0);
+  delay(1000);
+  color (255, 255, 255);
+  delay(1000);
+  color (128, 0, 255);
+  delay(1000);
+  color (0, 0, 0);
+  delay(1000);
+}
+
+void color (unsigned char red, unsigned char green, unsigned char blue) {
+  digitalWrite (redPin, red);
+  digitalWrite (bluePin, blue);
+  digitalWrite (greenPin, green);
+}
