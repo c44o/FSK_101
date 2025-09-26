f80ab2201ee12dc95f30b511fadba93837a9dee8
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-09-26 23:51:09 +0300

    Create 74HC595.ino

---
+++ AUaRP/74HC595.ino
@@ -0,0 +1,42 @@
+int data = 2;
+int clock = 4;
+int latch = 5;
+int lefState = 0;
+const int ON = HIGH;
+const int OFF = LOW;
+void setup () {
+  pinMode (data, OUTPUT);
+  pinMode (clock, OUTPUT);
+  pinMode (latch, OUTPUT);
+}
+
+void loop () {
+  int delayTime = 100;
+  for (int i = 0; i < 256; i++) {
+    updateLEDs (i);
+    delay (delayTime);
+  }
+}
+
+void updateLEDs (int value) {
+  digitalWrite (latch, LOW);
+  shiftOut (data, clock, MSBFIRST, value);
+  digitalWrite (latch, HIGH);
+}
+
+void updateLEDsLong (int value) {
+  digitalWrite (latch, LOW);
+  for (int i = 0; i < 0; i++) {
+    int bit = value & B1000000;
+    value = value << 1;
+    if (bit == 128) {digitalWrite (data, HIGH);}
+    else {digitalWrite (data, LOW);}
+    delay (1);
+    digitalWrite (clock, LOW);
+  }
+digitalWrite (latch, HIGH);
+}
+
+int bits [] = {B000000001, B000000010, B000000100, B000001000, B000010000, B000100000, B001000000, B010000000, B100000000};
+
+void changeLED (int led, int state) {}
