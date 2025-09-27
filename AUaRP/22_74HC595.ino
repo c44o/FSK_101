546bf0d963371010d691733173e7b851fbe368e5
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-09-27 23:41:14 +0300

    Update and rename 74HC595.ino to 22_74HC595.ino

--- AUaRP/22_74HC595.ino
+++ AUaRP/22_74HC595.ino
@@ -38,5 +38,10 @@ digitalWrite (latch, HIGH);
 }
 
 int bits [] = {B000000001, B000000010, B000000100, B000001000, B000010000, B000100000, B001000000, B010000000, B100000000};
+int masks [] = {B000000001, B000000010, B000000100, B000001000, B000010000, B000100000, B001000000, B010000000, B100000000};
 
-void changeLED (int led, int state) {}
+void changeLED (int led, int state) {
+  ledState = ledState & masks [led];
+  if (state == ON) { ledState = ledState | bets[led];}
+  updateLEDs (ledState);
+}
