cc7ecd5fe43fc3c4bcd8fbcfe16a487c6bf5ddee
c44o <149115105+c44o@users.noreply.github.com>
2025-12-16 22:07:17 +0300

    Create 31_joystick_1.ino

---
+++ AUaRP/31_joystick_1.ino
@@ -0,0 +1,18 @@
+int value = 0;
+void setup () {
+  pinMode (7, INPUT);
+  Serial.begin (9600);
+}
+
+void loop () {
+  value = analogRead (0);
+  Serial.print ("X:");
+  Serial.print (value, DEC);
+  value = analogRead (1);
+  Serial.print (" | Y:");
+  Serial.print (value, DEC);
+  value = digitalRead (7);
+  Serial.print (" | Z:");
+  Serial.println (value, DEC);
+  delay (100);
+}
